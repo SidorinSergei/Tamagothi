2417f90093c02e16b45c53dc10c63a411286651c
SidorinSergei <95975705+SidorinSergei@users.noreply.github.com>
2024-02-29 22:27:51 +0300

    Update page_food.dart with new features

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:tamagothi/view/page/page_food.dart';
 import 'package:tamagothi/view/page/page_reg.dart';
 import 'package:tamagothi/view/page/shop_page.dart';
 import 'package:tamagothi/view/page/page_authoriz.dart';
-
+FoodPageModel model = FoodPageModel(petSkin: 'assets/pers/pers_1.png',foodValue: 20);
 void main() {
   runApp(const MyApp());
 }
@@ -16,20 +16,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
         '/': (context) => Registration(),
         '/login': (context) => Authorization(),
-      '/home': (context) => Scaffold(
-        body: PageView(
-          children: [
-            HomePage(),
-            ShopPage(),
-            FoodPage(),
-          ],
+        '/home': (context) => Scaffold(
+          body: PageView(
+            children: [
+              HomePage(),
+              ShopPage(),
+              FoodPage(model: model,),
+            ],
+          ),
         ),
-      ),
       },
     );
   }
--- lib/view/page/page_food.dart
+++ lib/view/page/page_food.dart
@@ -2,7 +2,29 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tamagothi/view/widgets/scale.dart';
 
-class FoodPage extends StatelessWidget {
+// Модель
+class FoodPageModel {
+  final String petSkin;
+  double foodValue;
+
+  FoodPageModel({required this.petSkin, required this.foodValue});
+
+  void addFood(double value) {
+    foodValue += value;
+    if (foodValue > 100) foodValue = 100;
+  }
+}
+
+class FoodPage extends StatefulWidget {
+  final FoodPageModel model;
+
+  FoodPage({required this.model});
+
+  @override
+  _FoodPageState createState() => _FoodPageState();
+}
+
+class _FoodPageState extends State<FoodPage> {
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
@@ -18,10 +40,102 @@ class FoodPage extends StatelessWidget {
         child: Center(
           child: Stack(
             children: <Widget>[
-              Positioned(left: screenSize.width*0.095, top: screenSize.height*0.25, child: Image.asset('assets/pers/pers_1.png',width: screenSize.width*0.83,height: screenSize.height*0.83,),),
-              HealthScale(value:53,size: 0.23,leftSize: 0.39, topSize: 0.822,petImage: Image.asset('assets/images/food.png',width: screenSize.width*0.23,height: screenSize.height*0.23,),),
+              // Питомец
+              Positioned(
+                left: screenSize.width * 0.095,
+                top: screenSize.height * 0.25,
+                child: Image.asset(
+                  widget.model.petSkin,
+                  width: screenSize.width * 0.83,
+                  height: screenSize.height * 0.83,
+                ),
+              ),
+              HealthScale(
+                value: widget.model.foodValue,
+                size: 0.23,
+                leftSize: 0.39,
+                topSize: 0.07,
+                petImage: Image.asset(
+                  'assets/images/food.png',
+                  width: screenSize.width * 0.23,
+                  height: screenSize.height * 0.23,
+                ),
+              ),
+              Positioned(
+                left: screenSize.width * 0.1,
+                top: screenSize.height * 0.82,
+                width: screenSize.width * 0.8,
+                height: screenSize.height * 0.13,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    FoodItem(
+                      imagePath: 'assets/images/food_1.png',
+                      onFoodTap: () {
+                        setState(() {
+                          widget.model.addFood(5);
+                        });
+                      },
+                    ),
+                    FoodItem(
+                      imagePath: 'assets/images/food_2.png',
+                      onFoodTap: () {
+                        setState(() {
+                          widget.model.addFood(8);
+                        });
+                      },
+                    ),
+                    FoodItem(
+                      imagePath: 'assets/images/food_3.png',
+                      onFoodTap: () {
+                        setState(() {
+                          widget.model.addFood(10);
+                        });
+                      },
+                    ),
+                    FoodItem(
+                      imagePath: 'assets/images/food_4.png',
+                      onFoodTap: () {
+                        setState(() {
+                          widget.model.addFood(12);
+                        });
+                      },
+                    ),
+                    FoodItem(
+                      imagePath: 'assets/images/food_5.png',
+                      onFoodTap: () {
+                        setState(() {
+                          widget.model.addFood(14);
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class FoodItem extends StatelessWidget {
+  final String imagePath;
+  final VoidCallback onFoodTap;
+
+  FoodItem({required this.imagePath, required this.onFoodTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onFoodTap,
+      child: Padding(
+        padding: EdgeInsets.all(2.0),
+        child: Image.asset(
+          imagePath,
+          width: 100,
+          height: 550,
         ),
       ),
     );
